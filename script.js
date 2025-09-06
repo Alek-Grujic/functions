6d120ffe8d9341336fa4e401ce4d459ca1979fdb
Alek-Grujic <alek.grujic001@outlook.com>
2025-09-06 09:06:35 +0200

    Passing by value example

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 "use strict";
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking("LH123");
+// createBooking("LH123", 1223);
+// console.log(bookings);
+
+// ----------------------------------
+
+const flight = "LH234";
+const jonas = {
+  name: "Jonas",
+  passport: 3434535,
 };
 
-createBooking("LH123");
-createBooking("LH123", 1223);
-console.log(bookings);
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH543";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 3434535) {
+    alert("Cheched in");
+  } else {
+    alert("Wrong passport");
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
