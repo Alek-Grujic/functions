4e8573c7cf455b3542bdfdebd77b46161863ed3d
Alek-Grujic <alek.grujic001@outlook.com>
2025-09-06 07:58:05 +0200

    Exercising ES6 default parameters

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("LH123", 1223);
+console.log(bookings);
