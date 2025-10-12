a04ab1d7a53a58b34c7ab1f2adcca5020e6b876d
Alek-Grujic <alek.grujic001@outlook.com>
2025-10-12 08:06:13 +0200

    feat: demonstrate closure with delayed execution in boardPassengers function

--- poll.js
+++ poll.js
@@ -126,3 +126,16 @@ function clousure() {
 let squareOfTwo = clousure();
 
 squareOfTwo();
+
+// second example
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+boardPassengers(180, 3);
